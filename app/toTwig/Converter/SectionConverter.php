226a491d1a2bb3273df58d12c613b8792e249c97
Jędrzej Skoczek <j.skoczek@createit.pl>
2018-08-29 15:06:38 +0200

    OXDEV-1375 Added conversion class for section tag
    Section tag is converted to twig for tag

---
+++ app/toTwig/Converter/SectionConverter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace toTwig\Converter;
+
+use toTwig\ConverterAbstract;
+
+class SectionConverter extends ConverterAbstract
+{
+
+    public function convert(\SplFileInfo $file, $content)
+    {
+        $contentReplacedOpeningTag = $this->replaceSectionOpeningTag($content);
+        $content = $this->replaceSectionClosingTag($contentReplacedOpeningTag);
+
+        return $content;
+    }
+
+    public function getPriority()
+    {
+        return 0;
+    }
+
+    public function getName()
+    {
+        return 'section';
+    }
+
+    public function getDescription()
+    {
+        return 'Convert smarty {section} to twig {for}';
+    }
+
+    private function replaceSectionOpeningTag($content)
+    {
+        $pattern = '/\[\{section\b\s*([^{}]+)?\}\]/';
+        $string = '{% for :name in :start..:loop %}';
+
+        return preg_replace_callback($pattern, function($matches) use ($string) {
+
+            $match = $matches[1];
+            $search = $matches[0];
+
+            $attr = $this->attributes($match);
+            if(!isset($attr['start'])) {
+                $attr['start'] = 0;
+            }
+            $replace = $attr;
+            $string = $this->vsprintf($string, $replace);
+
+            // Replace more than one space to single space
+            $string = preg_replace('!\s+!', ' ', $string);
+
+            return str_replace($search, $string, $search);
+
+        }, $content);
+    }
+
+    private function replaceSectionClosingTag($content)
+    {
+        $search = '#\[\{/section\s*\}\]#';
+        $replace = '{% endfor %}';
+        return preg_replace($search, $replace, $content);
+    }
+}
