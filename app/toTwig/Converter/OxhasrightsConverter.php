6ce006139676f8a45a1c9c5a29a8471168472492
Tomasz Kowalewski <t.kowalewski@createit.pl>
2018-08-31 12:14:52 +0200

    OXDEV-1375 Convert OXID oxhasrights tag
    Converted OXID oxhasrights tag to custom Twig tag

---
+++ app/toTwig/Converter/OxhasrightsConverter.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace toTwig\Converter;
+
+use toTwig\ConverterAbstract;
+
+/**
+ * Class OxhasrightsConverter
+ *
+ * @author Tomasz Kowalewski (t.kowalewski@createit.pl)
+ */
+class OxhasrightsConverter extends ConverterAbstract
+{
+    /**
+     * @param \SplFileInfo $file
+     * @param string $content
+     *
+     * @return string
+     */
+    public function convert(\SplFileInfo $file, $content)
+    {
+        $content = $this->replaceOxhasrights($content);
+        $content = $this->replaceEndOxhasrights($content);
+
+        return $content;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPriority()
+    {
+        return 50;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'oxhasrights';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return 'Convert oxhasrights to twig';
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    private function replaceEndOxhasrights($content)
+    {
+        $search = "#\[\{/oxhasrights\s*\}\]#";
+        $replace = "{% endoxhasrights %}";
+
+        return preg_replace($search, $replace, $content);
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    private function replaceOxhasrights($content)
+    {
+        $pattern = "#\[\{oxhasrights\b\s*([^{}]+)?\}\]#i";
+
+        return preg_replace_callback($pattern, function ($matches) {
+            $match = $matches[1];
+
+            $attr = $this->attributes($match);
+            $ident = $this->value($attr['ident']);
+
+            return sprintf("{%% oxhasrights %s %%}", $ident);
+        }, $content);
+    }
+}
---
+++ tests/converter/OxhasrightsConverterTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace sankar\ST\Tests\Converter;
+
+use PHPUnit\Framework\TestCase;
+use toTwig\Converter\OxhasrightsConverter;
+
+/**
+ * Class OxhasrightsConverterTest
+ *
+ * @author Tomasz Kowalewski (t.kowalewski@createit.pl)
+ */
+class OxhasrightsConverterTest extends TestCase
+{
+    /** @var OxhasrightsConverter */
+    protected $converter;
+
+    public function setUp()
+    {
+        $this->converter = new OxhasrightsConverter();
+    }
+
+    /**
+     * @covers \toTwig\Converter\OxhasrightsConverter::convert
+     *
+     * @dataProvider Provider
+     *
+     * @param $smarty
+     * @param $twig
+     */
+    public function testThatForIsConverted($smarty, $twig)
+    {
+        // Test the above cases
+        $this->assertSame($twig,
+            $this->converter->convert($this->getFileMock(), $smarty)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function Provider()
+    {
+        return [
+            [
+                "[{oxhasrights ident=\"TOBASKET\"}]\nfoo\n[{/oxhasrights}]",
+                "{% oxhasrights \"TOBASKET\" %}\nfoo\n{% endoxhasrights %}"
+            ]
+        ];
+    }
+
+    /**
+     * @covers \toTwig\Converter\ForConverter::getName
+     */
+    public function testThatHaveExpectedName()
+    {
+        $this->assertEquals('oxhasrights', $this->converter->getName());
+    }
+
+    /**
+     * @covers \toTwig\Converter\OxhasrightsConverter::getDescription
+     */
+    public function testThatHaveDescription()
+    {
+        $this->assertNotEmpty($this->converter->getDescription());
+    }
+
+    /**
+     * @return \SplFileInfo
+     */
+    private function getFileMock()
+    {
+        /** @var \SplFileInfo $mock */
+        $mock = $this->getMockBuilder('\SplFileInfo')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        return $mock;
+    }
+}
