82d92d90f3f007f47d4196837053ec3da9d55659
Jędrzej Skoczek <j.skoczek@createit.pl>
2018-09-14 17:16:39 +0200

    OXDEV-1375 Added Acceptance tests
    Also tweaked Capture converter (fixed issued after merge)

--- tests/acceptance/AcceptanceTest.php
+++ tests/acceptance/AcceptanceTest.php
@@ -14,31 +14,64 @@ use toTwig\Console\Command\ConvertCommand;
 
 class AcceptanceTest extends TestCase
 {
-    private $tplPath;
-    private $twigPath;
+    private $templateNames;
 
     public function setUp()
     {
         parent::setUp();
-        $this->tplPath = dirname(__FILE__) . '/order.tpl';
-        $this->twigPath = dirname(__FILE__) . '/order.twig';
-        if(file_exists($this->twigPath)) {
-            unlink($this->twigPath);
+        $this->templateNames = ['basket', 'order', 'payment', 'thankyou', 'user'];
+        $this->unlinkConvertedFiles();
+    }
+
+    public function testConvert()
+    {
+        foreach($this->templateNames as $templateName) {
+            $this->convert($templateName);
         }
     }
 
-    public function test()
+    private function convert($templateName)
     {
         $command = new ConvertCommand();
         $commandTester = new CommandTester($command);
-        $commandTester->execute(['path' => $this->tplPath, '--ext' => 'twig']);
+        $commandTester->execute(['path' => $this->getSmartyTemplatePath($templateName), '--ext' => 'twig']);
 
-        $this->assertTrue(file_exists($this->twigPath));
+        $this->assertTrue(file_exists($this->getTwigTemplatePath($templateName)));
+        $actualTemplate = file_get_contents($this->getTwigTemplatePath($templateName));
+        $expectedTemplate = file_get_contents($this->getExpectedTwigTemplatePath($templateName));
+
+        $this->assertEquals($expectedTemplate, $actualTemplate);
+
+    }
+
+    private function getTwigTemplatePath($templateName)
+    {
+        return dirname(__FILE__) . '/' . $templateName . '.twig';
+    }
+
+    private function getSmartyTemplatePath($templateName)
+    {
+        return dirname(__FILE__) . '/' . $templateName . '.tpl';
+    }
+
+    private function getExpectedTwigTemplatePath($templateName)
+    {
+        return dirname(__FILE__) . '/' . $templateName . '-expected.twig';
+    }
+
+    private function unlinkConvertedFiles()
+    {
+        foreach($this->templateNames as $templateName) {
+            $twigTemplatePath = $this->getTwigTemplatePath($templateName);
+            if(file_exists($twigTemplatePath)) {
+                unlink($twigTemplatePath);
+            }
+        }
     }
 
     public function tearDown()
     {
         parent::tearDown();
-        unlink($this->twigPath);
+        $this->unlinkConvertedFiles();
     }
 }
