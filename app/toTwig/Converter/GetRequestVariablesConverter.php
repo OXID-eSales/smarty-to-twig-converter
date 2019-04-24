dfd635b783f8ff7aeed23d6c8e31dc9669832d9b
Jedrzej Skoczek <j.skoczek@createit.pl>
2019-04-24 09:56:43 +0200

    OXDEV-1375 added converter for $smarty.cookie and $smarty.get. Twig uses custom getters instead of givin full access to php global variables

---
+++ app/toTwig/Converter/GetRequestVariablesConverter.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace toTwig\Converter;
+
+/**
+ * Class GetRequestVariablesConverter
+ *
+ * @package toTwig\Converter
+ * @author  Jędrzej Skoczek
+ */
+class GetRequestVariablesConverter extends ConverterAbstract
+{
+
+    protected $name = 'get_request_variables';
+    protected $description = 'Convert direct access to php request variables to using getters. Only $_COOKIE and $_GET are accessible';
+    protected $priority = 50;
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    public function convert(string $content): string
+    {
+        $convertedCookieGetter = $this->convertCookie($content);
+        $convertedGetters = $this->convertGet($convertedCookieGetter);
+
+        return $convertedGetters;
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    private function convertCookie(string $content): string
+    {
+        $pattern = '/\$smarty\.cookies\.([a-zA-Z0-9]+)/';
+        $getterName = 'get_global_cookie';
+
+        return $this->convertGetter($content, $pattern, $getterName);
+    }
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    private function convertGet(string $content): string
+    {
+        $pattern = '/\$smarty\.get\.([a-zA-Z0-9]+)/';
+        $getterName = 'get_global_get';
+
+        return $this->convertGetter($content, $pattern, $getterName);
+    }
+
+    /**
+     * @param $content
+     * @param $pattern
+     * @param $getterName
+     *
+     * @return string
+     */
+    private function convertGetter(string $content, string $pattern, string $getterName): string
+    {
+        return preg_replace_callback(
+            $pattern,
+            function ($matches) use ($getterName) {
+                $search = str_replace($matches[0], $getterName . '("' . $matches[1] . '")', $matches[0]);
+
+                return $search;
+            },
+            $content
+        );
+    }
+}
---
+++ tests/Unit/GetRequestVariablesConverterTest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace sankar\ST\Tests\Unit;
+
+use toTwig\Converter\GetRequestVariablesConverter;
+use PHPUnit\Framework\TestCase;
+
+class GetRequestVariablesConverterTest extends TestCase
+{
+
+    /** @var GetRequestVariablesConverter */
+    protected $converter;
+
+    public function setUp()
+    {
+        $this->converter = new GetRequestVariablesConverter();
+    }
+
+    /**
+     * @covers       \toTwig\Converter\AssignConverter::convert
+     *
+     * @dataProvider Provider
+     *
+     * @param $smarty
+     * @param $twig
+     */
+    public function testThatAssignIsConverted($smarty, $twig)
+    {
+        // Test the above cases
+        $this->assertSame(
+            $twig,
+            $this->converter->convert($smarty)
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function Provider()
+    {
+        return [
+            [
+                '[{$smarty.cookies.foo}]',
+                '[{get_global_cookie("foo")}]'
+            ],
+            [
+                '[{ $smarty.cookies.foo }]',
+                '[{ get_global_cookie("foo") }]'
+            ],
+            [
+                '[{if $oView->isEnabled() && $smarty.cookies.foo != \'1\'}]',
+                '[{if $oView->isEnabled() && get_global_cookie("foo") != \'1\'}]'
+            ],
+            [
+                '[{$smarty.get.foo}]',
+                '[{get_global_get("foo")}]'
+            ],
+            [
+                '[{if $oView->isEnabled() && $smarty.get.foo != \'1\'}]',
+                '[{if $oView->isEnabled() && get_global_get("foo") != \'1\'}]'
+            ],
+            [
+                '[{$smarty.get.foo $smarty.cookies.foo}]',
+                '[{get_global_get("foo") get_global_cookie("foo")}]'
+            ],
+            [
+                '[{$smarty.cookies.foo $smarty.get.foo}]',
+                '[{get_global_cookie("foo") get_global_get("foo")}]'
+            ]
+        ];
+    }
+}
