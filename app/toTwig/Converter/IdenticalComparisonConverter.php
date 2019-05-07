42c03f52b00d3578e056a7b83a85249e6d24eba0
Jędrzej Skoczek <j.skoczek@createit.pl>
2019-05-07 16:14:27 +0200

    OXDEV-1375 fixed converstion of identical comparison operator
    '===' is translated to 'is same as()' function

---
+++ app/toTwig/Converter/IdenticalComparisonConverter.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace toTwig\Converter;
+
+/**
+ * Class IdenticalComparisonConverter
+ *
+ * @package toTwig\Converter
+ * @author  Jędrzej Skoczek
+ */
+class IdenticalComparisonConverter extends ConverterAbstract
+{
+
+    protected $name = 'identical_comparison_converter';
+    protected $description = 'Convert php "===" to twig "is same as()"';
+    protected $priority = 49;
+
+    /**
+     * @param string $content
+     *
+     * @return string
+     */
+    public function convert(string $content): string
+    {
+        /**
+         * for {% if "foo"==="foo bar" %}
+         *  $match[0] should contain left side of comparison with '===' sign i.e '=== "foo bar"'
+         *  $match[1] should only contain value, without twig closing bracket or following spaces i.e. "foo bar"
+         * regex is designed to capture string between comparison '===' and twig logic operators (and, or, not) and twig closing tag %}
+         */
+        $pattern = '/\s*\=\=\=+\s*((?:(?!\%\}|\s\%\}|\sand|\sor|\snot).)+)?/';
+
+        return preg_replace_callback(
+            $pattern,
+            function ($matches) {
+                $search = str_replace($matches[0], ' is same as(' . $matches[1] . ')', $matches[0]);
+
+                return $search;
+            },
+            $content
+        );
+    }
+}
---
+++ tests/Unit/IdenticalComparisonConverterTest.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace sankar\ST\Tests\Unit;
+
+use toTwig\Converter\IdenticalComparisonConverter;
+use PHPUnit\Framework\TestCase;
+
+class IdenticalComparisonConverterTest extends TestCase
+{
+
+    /** @var IdenticalComparisonConverter */
+    protected $converter;
+
+    public function setUp()
+    {
+        $this->converter = new IdenticalComparisonConverter();
+    }
+
+    /**
+     * @covers       \toTwig\Converter\IdenticalComparison::convert
+     * @dataProvider Provider
+     */
+    public function testThatIfIsConverted($smarty, $twig)
+    {
+        // Test the above cases
+        $this->assertSame(
+            $twig,
+            $this->converter->convert($smarty)
+        );
+    }
+
+    public function Provider()
+    {
+        return [
+            [
+                '1 === 1',
+                '1 is same as(1)'
+            ],
+            [
+                '1===1',
+                '1 is same as(1)'
+            ],
+            [
+                '1 === "foo"',
+                '1 is same as("foo")'
+            ],
+            [
+                '1==="foo"',
+                '1 is same as("foo")'
+            ],
+            [
+                '"foo" === "bar"',
+                '"foo" is same as("bar")'
+            ],
+            [
+                '"foo"==="bar"',
+                '"foo" is same as("bar")'
+            ],
+            [
+                '{% if "foo" === "bar" %}',
+                '{% if "foo" is same as("bar") %}'
+            ],
+            [
+                '{% if "foo"==="bar" %}',
+                '{% if "foo" is same as("bar") %}'
+            ],
+            [
+                '{% if "foo" === "bar" %}',
+                '{% if "foo" is same as("bar") %}'
+            ],
+            [
+                '{% if "foo"==="bar" %}',
+                '{% if "foo" is same as("bar") %}'
+            ],
+            [
+                '{% if "foo"==="foo bar" %}',
+                '{% if "foo" is same as("foo bar") %}'
+            ],
+            [
+                '{% if $oView->isConfirmAGBError() === 1 %}',
+                '{% if $oView->isConfirmAGBError() is same as(1) %}'
+            ],
+            [
+                '{%if $oView->isConfirmAGBError() === 1%}',
+                '{%if $oView->isConfirmAGBError() is same as(1)%}'
+            ],
+            [
+                '{% block checkout_order_errors %}
+        {% if oView.isConfirmAGBError() === 1 %}
+            {% include "message/error.html.twig" with {statusMessage: "READ_AND_CONFIRM_TERMS"|translate} %}',
+                '{% block checkout_order_errors %}
+        {% if oView.isConfirmAGBError() is same as(1) %}
+            {% include "message/error.html.twig" with {statusMessage: "READ_AND_CONFIRM_TERMS"|translate} %}'
+            ],
+            [
+                '<li{%if $oContent->oxcontents__oxloadid->value === $oTopCont->oxcontents__oxloadid->value%} class="active"{%/if%}>',
+                '<li{%if $oContent->oxcontents__oxloadid->value is same as($oTopCont->oxcontents__oxloadid->value)%} class="active"{%/if%}>'
+            ],
+            [
+                '{%if (foo === false and bar === false) or foobar === "foobar" not foo === true %}',
+                '{%if (foo is same as(false) and bar is same as(false)) or foobar is same as("foobar") not foo is same as(true) %}'
+            ]
+        ];
+    }
+
+    /**
+     * @covers \toTwig\Converter\IdenticalComparison::getName
+     */
+    public function testThatHaveExpectedName()
+    {
+        $this->assertEquals('identical_comparison_converter', $this->converter->getName());
+    }
+
+    /**
+     * @covers \toTwig\Converter\IdenticalComparison::getDescription
+     */
+    public function testThatHaveDescription()
+    {
+        $this->assertNotEmpty($this->converter->getDescription());
+    }
+}
